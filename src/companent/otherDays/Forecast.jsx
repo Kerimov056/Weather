a464d13b4571a66f153ff4fe9e0aaac7252257f4
Kerimov056 <ulvisk@code.edu.az>
2023-05-18 15:51:34 -0700

    Accordion info

--- src/companent/otherDays/Forecast.jsx
+++ src/companent/otherDays/Forecast.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 import './forecast.scss'
 import {Accordion,AccordionItem,AccordionItemHeading,AccordionItemButton,AccordionItemPanel} from "react-accessible-accordion";
 
+{/* Accordion nedir?  Accordionlar hazir bir funkisyalardir! */}
+{/* Accordion :her hansisa bir divi daraltmasini saglamak icin bir companentdir. */}
+{/* AccordionItem :Accordion icindekilerdeki companentdir.Yeni Accordion'a - bir mezmun desek AccordionItem-onun hisseleridir */}
+{/* AccordionItemHeading :Bu basliq bolmesidir basliq bolmesini temsil edir */}
+{/* AccordionItemButton :Bu AccordionItemHeading'la istifade olunan bir buttondur.Button'a click edende yazdiginiz hadise bas verir*/}
+{/* AccordionItemPanel :Bu'da AccordionItemButton click etdiyimiz zaman acilan companentdir */}
+
+{/* Accordion allowZeroExpanded :Normaldda Accordion ne edirdi ? Accordion-bir companent acilanda digerlerinin baglanmasini  
+    sagalyirdi. Yalniz allowZeroExpanded <-- biz bu prop'u daxil etsek bu bize companentlerin ister hamisin aciq 
+    ister hamisin bagli tutmasini saglayir !!!
+*/}
+
+
 
 const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 
 const Forecast = ({data}) => {
     const toDay = new Date().getDay()
-    const clack = WEEK_DAYS.splice(toDay,WEEK_DAYS.length).concat(WEEK_DAYS.slice(0,toDay));
+    let clack = WEEK_DAYS.splice(toDay,WEEK_DAYS.length).concat(WEEK_DAYS.slice(0,toDay));
 
   return (
     <>
